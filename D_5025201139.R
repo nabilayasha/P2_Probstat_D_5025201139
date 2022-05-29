409c129282a26518f9c19303c487a1298503ecf6
nabilayasha <74358409+nabilayasha@users.noreply.github.com>
2022-05-29 21:26:47 +0700

    Update D_5025201139.R

--- D_5025201139.R
+++ D_5025201139.R
@@ -1,4 +1,12 @@
 #No1
+n <- c(1,2,3,4,5,6,7,8,9)
 x <- c(78,75,67,77,70,72,78,74,77)
 y <- c(100,95,70,90,90,90,89,90,100)
 
+#A
+data <- data.frame(n,X,Y)
+selisih = (x-y)
+standardev = sd(selisih)
+print (selisih)\
+
+#B
